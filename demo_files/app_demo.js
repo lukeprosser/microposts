7705a6791a92731ab14a75263de0147950d11802
Luke Prosser <luke.prosser@cloud-iq.com>
2019-02-03 16:18:10 +0000

    first commit

---
+++ demo_files/app_demo.js
@@ -0,0 +1,13 @@
+// CommonJS module syntax
+// const person = require('./mymodule1');
+
+// ES2015 module
+// import { person, sayHello } from './mymodule2';
+// import * as mod from './mymodule2';
+import greeting from './mymodule2';
+
+// console.log(mod.person.name);
+
+// console.log(mod.sayHello());
+
+console.log(greeting);
---
+++ demo_files/mymodule1.js
@@ -0,0 +1,4 @@
+module.exports = {
+  name: 'Luke',
+  email: 'test@test.com'
+}
